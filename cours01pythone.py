77f2c513a44e435c3deae327f79f3973d7a3bcc1
vipar_da7i7 <117691465+medamine181@users.noreply.github.com>
2024-06-13 12:37:05 +0100

    Add files via upload

---
+++ cours01pythone.py
@@ -0,0 +1,95 @@
+# # cours pythone update .
+# # tabile pythone = \t .
+# print ("test 1 : tabile")
+# print ("name\tprname\tage")
+# print ("----------------------------------")
+# print ("john\tjohnson\t25")
+# print ("----------------------------------")
+# print ("jane\tjones\t30")
+# print ("")
+# print ("/////////////////////////////////////////")
+# print ("")
+# # none python = \n .
+# print ("test 2 : none")
+# print ("")
+# print ("name :\nprname :")
+# # noneacone python = \ .
+# print ('')
+# print ("test 3 : noneacone")
+# print ("")
+# print ("\"med\" , \"amine\"")
+# # risolte == "med" , "amine" .
+# # 
+# # lower and upper and len  .
+# print ("lower and upper")
+# print ("")
+# print ("MED".lower().islower())
+# print ("amine".upper().isupper())
+# print ("")
+# # len 
+# rt = "medamine"
+# print (len(rt))
+# print (rt[2])
+# # index  and  replace
+# print (rt.index("d"))
+# print (rt.replace("med" , "mohamed "))
+# # number for python 
+# my_num = -10 
+# print (str (my_num) + " my age old")
+# print (abs (my_num))
+# print (pow (2,3)) # 2^3
+# print (max(5 , 2)) #5
+# print (min(5 , 2)) #2
+# print (round(3.7)) #4
+# from math import *
+# print (floor(5.9)) #5
+# print (ceil(5.9)) #6
+# print (sqrt(9)) #3
+# name = input("Enter your name : ")
+# age = int(input("Enter your age : "))
+# dataage = 2024 - age
+# print("Hello " + name + "!" + " you are " , age , " and they were born " , dataage )
+
+# # objacket anre !
+# franed = ["ahmid" , "beassam" , "chayma" , "douaa" , "emani" , "mohamed"]
+# franed[5] = "med"
+# ahmid = {
+#     "name" : "ahmid" ,
+#     "prname" : "bin abdalah" ,
+#     "age" : 25 ,
+#     "city" : "tunis" ,
+#     "email" : "bin1abdalahahmid12@gmail.com"
+# }
+# print (franed[0 : 3])
+# print (ahmid["name"])
+# print (ahmid["prname"])
+# print (franed[5])
+
+# # function !
+# mita = ["facebook" , "instagram" , "twitar" ]
+# print(mita)
+
+# age = [65 , 24 , 50 , 8 , 2]
+# mita.extend(age)
+# print(mita)
+
+# mita.append('youtube')
+# print(mita)
+
+# mita.insert(2 , "email.com")
+# print(mita)
+
+# mita.remove("instagram")
+# print(mita)
+
+# mita.pop()
+# print(mita)
+
+# mita.clear()
+# print(mita)
+
+# age.sort()
+# print (age)
+
+
+
---
+++ cours02pythone.py
@@ -0,0 +1,13 @@
+# funtion "def" $
+def say_hi (name , age):
+    print("Hello " + name + " you are " + age )
+
+print ("****")
+say_hi("amine" , '18' )
+say_hi("bessam" , '31' )
+print ("****")
+
+
+
+
+
